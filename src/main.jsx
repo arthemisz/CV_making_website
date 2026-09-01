de1384013773355ad24eccb30efd629d0f98ccf5
Arthemisz <melestically@gmail.com>
2026-09-02 00:31:52 +0300

    Update index page
    Co-authored-by: qweinten <melekte.ermias@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,14 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> 37e18cdab30a2ea901a19896f2b5d91132deda84
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -16,4 +8,3 @@ export default defineConfig({
     outDir: 'dist',
   }
 })
->>>>>>> 37e18cdab30a2ea901a19896f2b5d91132deda84
